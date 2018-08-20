350b9fe95285ef38df16f8bc188fb38e1d5f8345
vlad <vlad@notebook.lan>
2018-08-20 03:37:45 +0300

    Add gcode sender script

---
+++ sender.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+import time
+import getopt
+import sys
+import serial
+
+baudrate = 57600
+port = "/dev/ttyUSB0"
+f = None
+
+def usage():
+    print("Usage: sender.py [-h] [-b baudrate] [-p serial port] -f gcode_file")
+
+try:
+    optlist, args = getopt.getopt(sys.argv[1:], "hf:p:b:")
+except getopt.GetoptError as err:
+    usage()
+    sys.exit(1)
+
+for o, a in optlist:
+    if o == "-h":
+        usage()
+        sys.exit(0)
+    elif o == "-f":
+        f = a
+    elif o == "-b":
+        baudrate = int(a)
+    elif o == "-p":
+        port = a
+    else:
+        usage()
+        sys.exit(1)
+if f == None:
+    usage()
+    sys.exit(1)
+
+ser = serial.Serial(port, baudrate, bytesize=8, parity='N', stopbits=1)
+
+source = open(f, "r")
+gcoderaw = source.readlines()
+source.close()
+
+gcode = []
+
+for gc in gcoderaw:
+    gc = gc.strip("\n")
+    gc = gc.strip("\r")
+    if len(gc) > 0:
+        gcode.append(gc)
+
+for gc in gcode:
+    print("Sending: %s" % gc)
+    cmd = gc + "\n"
+    ser.write(bytes(cmd, "UTF-8"))
+    time.sleep(0.5)
+
+ser.close()
+
