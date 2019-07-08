8f5f359b8e7199fad023a2f5a2dff0eabfe430ad
Vladislav Tsendrovskii <vlad@vltc.net.eu.org>
2019-07-09 01:33:30 +0300

    Fix serial io

--- core/shell/serial_io/serial_io.c
+++ core/shell/serial_io/serial_io.c
@@ -13,7 +13,7 @@ static unsigned char inbuf[BUFLEN];
 static int inlen;
 
 static unsigned char outbuf[BUFLEN + 2];
-static int outlen;
+static int outlast;
 static int outpos;
 
 static void (*cb_line_received)(const unsigned char *, size_t);
@@ -33,7 +33,7 @@ void serial_io_init(void)
 {
     opts.rdy = 1;
     outpos = 0;
-    outlen = 0;
+    outlast = 0;
 }
 
 static void serial_io_char_received(unsigned char c)
@@ -57,26 +57,27 @@ static void serial_io_start_transmit(void)
 {
     if (opts.rdy == 1)
     {
-        outpos = 1;
         opts.rdy = 0;
+        int p = outpos;
+        outpos += 1;
+        outpos %= sizeof(outbuf);
         if (cb_byte_transmit)
-            cb_byte_transmit(outbuf[0]);
+            cb_byte_transmit(outbuf[p]);
     }
 }
 
 static void serial_io_char_transmitted(void)
 {
-    if (outbuf[outpos] && outpos < outlen)
+    if (outpos != outlast)
     {
         int p = outpos;
-        outpos++;
+        outpos += 1;
+        outpos %= sizeof(outbuf);
         if (cb_byte_transmit)
             cb_byte_transmit(outbuf[p]);
     }
     else
     {
-        outlen = 0;
-        outpos = 0;
         opts.rdy = 1;
         cb_sended();
     }
@@ -84,13 +85,22 @@ static void serial_io_char_transmitted(void)
 
 static void send_buffer(const unsigned char *buf, size_t len)
 {
-    strncpy(outbuf, buf, len);
-    len = strlen(outbuf);
-    outbuf[len] = '\n';
-    outbuf[len+1] = '\r';
-    outlen = len + 2;
-    outpos = 0;
-    serial_io_start_transmit();
+    int i;
+    for (i = 0; i < len; i++)
+    {
+        outbuf[outlast] = buf[i];
+        outlast += 1;
+        outlast %= sizeof(outbuf);
+    }
+    outbuf[outlast] = '\n';
+    outlast += 1;
+    outlast %= sizeof(outbuf);
+    outbuf[outlast] = '\r';
+    outlast += 1;
+    outlast %= sizeof(outbuf);
+
+    if (opts.rdy == 1)
+        serial_io_start_transmit();
 }
 
 static void register_received_cb(void (*f)(const unsigned char *, size_t))
