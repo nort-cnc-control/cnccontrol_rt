b377301b7df39258eeca8769bb330cb80ed708b5
Vladislav Tsendrovskii <vlad@vltc.net.eu.org>
2021-04-30 19:30:45 +0300

    Fix CS

--- libs/libip/net.c
+++ libs/libip/net.c
@@ -13,15 +13,17 @@
 static uint8_t local_mac[6];
 static uint32_t local_ip;
 
-static struct packet_state_s {
-	uint8_t  remote_mac[6];
+static struct packet_state_s
+{
+	uint8_t remote_mac[6];
 	uint32_t remote_ip;
 } packet_state;
 
 static uint8_t send_buffer[ETHERNET_MTU];
 
 #define ARP_RECORDS 10
-static struct apr_record {
+static struct apr_record
+{
 	int8_t ttl;
 	uint8_t mac[6];
 	uint32_t ip;
@@ -30,7 +32,7 @@ static const uint8_t mac_bcast[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 
 typedef const uint8_t *mac_ptr;
 
-static void find_mac(const uint32_t ip, mac_ptr* mac)
+static void find_mac(const uint32_t ip, mac_ptr *mac)
 {
 	int i;
 	*mac = mac_bcast;
@@ -114,8 +116,8 @@ void libip_send_icmp_echo_reply(const uint8_t *payload, size_t len, const uint32
 
 	size_t payload_len = icmp_fill_echo_payload(buffer + ETHERNET_HEADER_LEN + IP_HEADER_LEN + ICMP_HEADER_LEN, payload, len);
 	size_t echo_len = icmp_fill_echo_header(buffer + ETHERNET_HEADER_LEN + IP_HEADER_LEN + ICMP_HEADER_LEN, id, seq, payload_len);
-    size_t icmp_len = icmp_fill_header(buffer + ETHERNET_HEADER_LEN + IP_HEADER_LEN, ICMP_TYPE_ECHO_REPLY, 0, echo_len);
-    size_t ip_len = ip_fill_header(buffer + ETHERNET_HEADER_LEN, local_ip, remote_ip, IP_PROTOCOL_ICMP, 30, icmp_len);
+	size_t icmp_len = icmp_fill_header(buffer + ETHERNET_HEADER_LEN + IP_HEADER_LEN, ICMP_TYPE_ECHO_REPLY, 0, echo_len);
+	size_t ip_len = ip_fill_header(buffer + ETHERNET_HEADER_LEN, local_ip, remote_ip, IP_PROTOCOL_ICMP, 30, icmp_len);
 	size_t eth_len = ethernet_fill_header(buffer, local_mac, remote_mac, ETHERTYPE_IP, ip_len);
 	send_ethernet_frame(send_buffer, eth_len);
 }
@@ -123,18 +125,18 @@ void libip_send_icmp_echo_reply(const uint8_t *payload, size_t len, const uint32
 void libip_send_arp_response(const uint32_t remote_ip, const uint8_t remote_mac[6])
 {
 	memset(send_buffer, 0, sizeof(send_buffer));
-	uint8_t remote_ip_buf[4] = {(uint8_t)(remote_ip>>24), (uint8_t)(remote_ip>>16), (uint8_t)(remote_ip>>8), (uint8_t)(remote_ip)};
-	uint8_t local_ip_buf[4] = {(uint8_t)(local_ip>>24), (uint8_t)(local_ip>>16), (uint8_t)(local_ip>>8), (uint8_t)(local_ip)};
-	
+	uint8_t remote_ip_buf[4] = {(uint8_t)(remote_ip >> 24), (uint8_t)(remote_ip >> 16), (uint8_t)(remote_ip >> 8), (uint8_t)(remote_ip)};
+	uint8_t local_ip_buf[4] = {(uint8_t)(local_ip >> 24), (uint8_t)(local_ip >> 16), (uint8_t)(local_ip >> 8), (uint8_t)(local_ip)};
+
 	uint8_t *buffer = send_buffer;
 	arp_set_sender_hardware(buffer + ETHERNET_HEADER_LEN, local_mac, 6, 4);
-    arp_set_sender_protocol(buffer + ETHERNET_HEADER_LEN, local_ip_buf, 6, 4);
+	arp_set_sender_protocol(buffer + ETHERNET_HEADER_LEN, local_ip_buf, 6, 4);
 
-    arp_set_target_hardware(buffer + ETHERNET_HEADER_LEN, remote_mac, 6, 4);
-    arp_set_target_protocol(buffer + ETHERNET_HEADER_LEN, remote_ip_buf, 6, 4);
+	arp_set_target_hardware(buffer + ETHERNET_HEADER_LEN, remote_mac, 6, 4);
+	arp_set_target_protocol(buffer + ETHERNET_HEADER_LEN, remote_ip_buf, 6, 4);
 
-    size_t arp_len = arp_fill_header(buffer + ETHERNET_HEADER_LEN, ARP_HTYPE_ETHERNET, 6, ETHERTYPE_IP, 4, ARP_OPERATION_RESPONSE);
-    size_t eth_len = ethernet_fill_header(buffer, local_mac, remote_mac, ETHERTYPE_ARP, arp_len);
+	size_t arp_len = arp_fill_header(buffer + ETHERNET_HEADER_LEN, ARP_HTYPE_ETHERNET, 6, ETHERTYPE_IP, 4, ARP_OPERATION_RESPONSE);
+	size_t eth_len = ethernet_fill_header(buffer, local_mac, remote_mac, ETHERTYPE_ARP, arp_len);
 
 	send_ethernet_frame(send_buffer, eth_len);
 }
@@ -144,128 +146,128 @@ void libip_send_arp_response(const uint32_t remote_ip, const uint8_t remote_mac[
 static void handle_udp(const uint8_t *payload, size_t len)
 {
 	uint16_t sport = udp_get_source(payload, len);
-    uint16_t dport = udp_get_destination(payload, len);
-    size_t udp_payload_len;
-    const uint8_t *udp_payload = udp_get_payload(payload, len, &udp_payload_len);
+	uint16_t dport = udp_get_destination(payload, len);
+	size_t udp_payload_len;
+	const uint8_t *udp_payload = udp_get_payload(payload, len, &udp_payload_len);
 
-    udp_packet_handler(packet_state.remote_ip, dport, sport, udp_payload, udp_payload_len);
+	udp_packet_handler(packet_state.remote_ip, dport, sport, udp_payload, udp_payload_len);
 }
 
 static void handle_icmp(const uint8_t *payload, size_t len)
 {
-    uint8_t type = icmp_get_type(payload, len);
-    uint8_t code = icmp_get_code(payload, len);
-    switch (type)
-    {
-        case ICMP_TYPE_ECHO:
-        {
-            uint16_t id = icmp_echo_get_identifier(payload, len);
-            uint16_t sn = icmp_echo_get_sequence_number(payload, len);
-            size_t icmp_payload_len;
-            const uint8_t *icmp_payload = icmp_echo_get_payload(payload, len, &icmp_payload_len);
+	uint8_t type = icmp_get_type(payload, len);
+	uint8_t code = icmp_get_code(payload, len);
+	switch (type)
+	{
+	case ICMP_TYPE_ECHO:
+	{
+		uint16_t id = icmp_echo_get_identifier(payload, len);
+		uint16_t sn = icmp_echo_get_sequence_number(payload, len);
+		size_t icmp_payload_len;
+		const uint8_t *icmp_payload = icmp_echo_get_payload(payload, len, &icmp_payload_len);
 
-            /* Send ICMP ECHO REPLY response */
-			libip_send_icmp_echo_reply(icmp_payload, icmp_payload_len, packet_state.remote_ip, id, sn);
-            break;
-        }
-        default:
-            break;
-    }
+		/* Send ICMP ECHO REPLY response */
+		libip_send_icmp_echo_reply(icmp_payload, icmp_payload_len, packet_state.remote_ip, id, sn);
+		break;
+	}
+	default:
+		break;
+	}
 }
 
 static void handle_ip(const uint8_t *payload, size_t len)
 {
-    uint8_t protocol = ip_get_protocol(payload, len);
-    packet_state.remote_ip = ip_get_source(payload, len);
+	uint8_t protocol = ip_get_protocol(payload, len);
+	packet_state.remote_ip = ip_get_source(payload, len);
 	remember_mac(packet_state.remote_ip, packet_state.remote_mac);
 
-    size_t ip_payload_len;
-    const uint8_t *ip_payload = ip_get_payload(payload, len, &ip_payload_len);
-    switch (protocol)
-    {
-        case IP_PROTOCOL_UDP:
-        {
-            handle_udp(ip_payload, ip_payload_len);
-            break;
-        }
-        case IP_PROTOCOL_ICMP:
-        {
-            handle_icmp(ip_payload, ip_payload_len);
-            return;
-        }
-        default:
-            break;
-    }
+	size_t ip_payload_len;
+	const uint8_t *ip_payload = ip_get_payload(payload, len, &ip_payload_len);
+	switch (protocol)
+	{
+	case IP_PROTOCOL_UDP:
+	{
+		handle_udp(ip_payload, ip_payload_len);
+		break;
+	}
+	case IP_PROTOCOL_ICMP:
+	{
+		handle_icmp(ip_payload, ip_payload_len);
+		return;
+	}
+	default:
+		break;
+	}
 }
 
 static void handle_arp(const uint8_t *payload, size_t len)
 {
-    uint16_t hw = arp_get_hardware(payload, len);
-    uint16_t proto = arp_get_protocol(payload, len);
+	uint16_t hw = arp_get_hardware(payload, len);
+	uint16_t proto = arp_get_protocol(payload, len);
 
-    if (hw != ARP_HTYPE_ETHERNET || proto != ETHERTYPE_IP)
-        return;
+	if (hw != ARP_HTYPE_ETHERNET || proto != ETHERTYPE_IP)
+		return;
 
-    size_t hwlen;
-    size_t plen;
+	size_t hwlen;
+	size_t plen;
 
-    uint8_t sender_hw[6]; 
-    memcpy(sender_hw, arp_get_sender_hardware(payload, len, &hwlen), sizeof(sender_hw));
+	uint8_t sender_hw[6];
+	memcpy(sender_hw, arp_get_sender_hardware(payload, len, &hwlen), sizeof(sender_hw));
 
-    uint8_t sender_p[4];
-    memcpy(sender_p, arp_get_sender_protocol(payload, len, &plen), sizeof(sender_p));
+	uint8_t sender_p[4];
+	memcpy(sender_p, arp_get_sender_protocol(payload, len, &plen), sizeof(sender_p));
 
-    const uint8_t *target_hw = arp_get_target_hardware(payload, len, &hwlen);
-    const uint8_t *target_p = arp_get_target_protocol(payload, len, &plen);
+	const uint8_t *target_hw = arp_get_target_hardware(payload, len, &hwlen);
+	const uint8_t *target_p = arp_get_target_protocol(payload, len, &plen);
 
-    uint32_t sender_ip = (uint32_t)sender_p[0] << 24 | (uint32_t)sender_p[1] << 16 | (uint32_t)sender_p[2] << 8 | (uint32_t)sender_p[3];
-    uint32_t target_ip = (uint32_t)target_p[0] << 24 | (uint32_t)target_p[1] << 16 | (uint32_t)target_p[2] << 8 | (uint32_t)target_p[3];
+	uint32_t sender_ip = (uint32_t)sender_p[0] << 24 | (uint32_t)sender_p[1] << 16 | (uint32_t)sender_p[2] << 8 | (uint32_t)sender_p[3];
+	uint32_t target_ip = (uint32_t)target_p[0] << 24 | (uint32_t)target_p[1] << 16 | (uint32_t)target_p[2] << 8 | (uint32_t)target_p[3];
 
-    uint16_t op = arp_get_operation(payload, len);
+	uint16_t op = arp_get_operation(payload, len);
 
-    switch (op)
-    {
-        case ARP_OPERATION_REQUEST:
-        {
-            if (target_ip == local_ip)
-            {
-                uint8_t *buffer = send_buffer;
-                uint8_t rshw[6], rsp[4];
+	switch (op)
+	{
+	case ARP_OPERATION_REQUEST:
+	{
+		if (target_ip == local_ip)
+		{
+			uint8_t *buffer = send_buffer;
+			uint8_t rshw[6], rsp[4];
 
-                /* Send ARP response */
-                libip_send_arp_response(sender_ip, sender_hw);
-            }
-            break;
-        }
-        case ARP_OPERATION_RESPONSE:
-        {
-            break;
-        }
-        default:
-            break;
-    }
+			/* Send ARP response */
+			libip_send_arp_response(sender_ip, sender_hw);
+		}
+		break;
+	}
+	case ARP_OPERATION_RESPONSE:
+	{
+		break;
+	}
+	default:
+		break;
+	}
 }
 
 void libip_handle_ethernet(const uint8_t *payload, size_t len)
 {
-    uint16_t ethertype = ethernet_get_ethertype(payload, len);
+	uint16_t ethertype = ethernet_get_ethertype(payload, len);
 
-    memcpy(packet_state.remote_mac, ethernet_get_source(payload, len), 6);
+	memcpy(packet_state.remote_mac, ethernet_get_source(payload, len), 6);
 
-    size_t ethernet_payload_len;
-    const uint8_t *ethernet_payload = ethernet_get_payload(payload, len, &ethernet_payload_len);
-    switch (ethertype)
-    {
-        case ETHERTYPE_IP:
-            handle_ip(ethernet_payload, ethernet_payload_len);
-            break;
-        case ETHERTYPE_ARP:
-            handle_arp(ethernet_payload, ethernet_payload_len);
-            break;
+	size_t ethernet_payload_len;
+	const uint8_t *ethernet_payload = ethernet_get_payload(payload, len, &ethernet_payload_len);
+	switch (ethertype)
+	{
+	case ETHERTYPE_IP:
+		handle_ip(ethernet_payload, ethernet_payload_len);
+		break;
+	case ETHERTYPE_ARP:
+		handle_arp(ethernet_payload, ethernet_payload_len);
+		break;
 
-        default:
-            break;
-    }
+	default:
+		break;
+	}
 }
 
 void libip_init(uint32_t ip, const uint8_t mac[6])
